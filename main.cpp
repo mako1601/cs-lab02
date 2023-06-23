fff8aff37da8b54872956429743a407f18157e6d
Alice (YuraB) <alice@example.com>
2023-06-23 17:54:40 +0300

    code: добавлен вывод разности

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
 	std::cout << "Enter A and B: ";
 	int a, b;
 	std::cin >> a >> b;
-	std::cout << "A + B = " << a + b << '\n';
+	std::cout << "A + B = " << a + b << '\n'
+			  << "A - B = " << a - b << '\n';
 }
